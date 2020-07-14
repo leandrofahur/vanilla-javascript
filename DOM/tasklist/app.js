203ec6cc32bfe31fd5ce878faa7e855de221a57e
leandrofahur <leandrofahur@gmail.com>
2020-07-14 02:02:11 -0700

    event listener to add a task

--- DOM/tasklist/app.js
+++ DOM/tasklist/app.js
@@ -1,6 +1,36 @@
 // UI vars:
-const task = document.getElementById('task');                        // input field
-const addTaskBtn = document.getElementById('add-task-btn');          // btn
-const taskFilter = document.getElementById('task-filter');           // input field
-const deleteTaskBtn = document.getElementById('delete-task-btn');    // btn
+const task = document.getElementById('task');                        
+const addTaskBtn = document.getElementById('add-task-btn');          
+const taskFilter = document.getElementById('task-filter');           
+const deleteTaskBtn = document.getElementById('delete-task-btn');    
+const taskList = document.querySelector('.collection');              
+
+
+
+// Load all events listeners:
+const loadAllEventsListeners = () => {
+  // Click on btn ( Add Task ):
+  addTaskBtn.addEventListener('click', onAddTaskBtnClick);
+}
+
+// Handle the ( Add Task ) button:
+const onAddTaskBtnClick = () => {
+  // Check if there is something in the field:
+  if(task.value === '') {
+    alert('Add a new task');
+  } else {
+    // console.log(task.value);
+    
+  }
+}
+
+
+loadAllEventsListeners();
+
+
+
+
+
+
+
 
