dd90808852b71e9b7510362fcaa84b848afbfc32
leandrofahur <leandrofahur@gmail.com>
2020-07-14 01:50:46 -0700

    v1.0 to-do list and app.js with UI vars

---
+++ DOM/tasklist/app.js
@@ -0,0 +1,6 @@
+// UI vars:
+const task = document.getElementById('task');                        // input field
+const addTaskBtn = document.getElementById('add-task-btn');          // btn
+const taskFilter = document.getElementById('task-filter');           // input field
+const deleteTaskBtn = document.getElementById('delete-task-btn');    // btn
+
