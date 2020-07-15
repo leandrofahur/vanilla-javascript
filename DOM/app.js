4f87db92372be0d53fdf9f866dd5993bfa2a6b1a
leandrofahur <leandrofahur@gmail.com>
2020-07-14 17:03:53 -0700

    get a NodeList with 'querySelectorAll'

--- DOM/app.js
+++ DOM/app.js
@@ -1,27 +1,35 @@
-// Change the title content:
-const h1 = document.querySelector('h1');
-// console.log(h1);
-// h1.textContent = 'Manipulating the DOM';
+// // Change the title content:
+// const h1 = document.querySelector('h1');
+// // console.log(h1);
+// // h1.textContent = 'Manipulating the DOM';
 
-// Grab the unordered-list element:
-const li = document.getElementsByTagName('li');
-console.log(li);
+// // Grab the unordered-list element:
+// const li = document.getElementsByTagName('li');
+// // console.log(li);
 
-li[0].textContent = 'First child'; 
-li[2].textContent = 'Last child'; 
+// li[0].textContent = 'First child'; 
+// li[2].textContent = 'Last child'; 
   
-// Grab from class:
-const card = document.querySelector('ul').getElementsByTagName('li');
-console.log(card);
+// // Grab from class:
+// const card = document.querySelector('ul').getElementsByTagName('li');
+// // console.log(card);
 
-// Transform the HTMLCollection into an array:
-const cardArray = Array.from(card);
-console.log(cardArray);
-cardArray.forEach((li, index) => {
+// // Transform the HTMLCollection into an array:
+// const cardArray = Array.from(card);
+// // console.log(cardArray);
+// cardArray.forEach((li, index) => {
+//   // console.log(li);
+//   li.textContent = `Child number ${index}`;
+// });
+
+// Nodelist: (querySelectorAll return a nodelist)
+const ul = document.querySelectorAll('ul li');
+console.log(ul)
+
+ul.forEach((li,index) => {
+  li.style.color = 'blue';
   console.log(li);
-  li.textContent = `Child number ${index}`;
-});
-
+})
 
 
 
