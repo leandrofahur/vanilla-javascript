31a0c0a3eb798bc15d258756b63b0cd62bea4f8d
leandrofahur <leandrofahur@gmail.com>
2020-07-14 23:35:07 -0700

    exhibit user and money

--- small-projects/array-methods/app.js
+++ small-projects/array-methods/app.js
@@ -15,16 +15,32 @@ getRandomUser();
 
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api/');
-  const data = await res.json();
-  const userData = data.results[0];
+  const dataResJson = await res.json();
+  const userData = dataResJson.results[0];
   addData({
     name: `${userData.name.first} ${userData.name.last}`,
-    money: Math.floor(Math.random() * 100000)
+    money: Math.floor(Math.random() * 1000000)
   });
 }
 
-const addData = (obj) => {
+function addData(obj) {
   data.push(obj);
-  // console.log(data);
+  onUpdateDOM();
 }
 
+function onUpdateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+};
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
