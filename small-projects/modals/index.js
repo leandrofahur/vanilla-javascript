e2c12816858d004590c304689049e407439d21df
leandrofahur <leandrofahur@gmail.com>
2020-07-17 18:03:30 -0700

    modals

--- small-projects/modals/index.js
+++ small-projects/modals/index.js
@@ -6,3 +6,19 @@ const modal = document.getElementById('modal');
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav');
 })
+
+open.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+  console.log(modal)
+})
+
+close.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+  console.log(modal)
+})
+
+window.addEventListener('click', (evt) => {
+  if(evt.target === modal) {
+    modal.classList.remove('show-modal');
+  }
+})
