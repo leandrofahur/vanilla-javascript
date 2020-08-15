b7653013a9a3429c653ff35f4e913dc3b6425b1c
leandrofahur <leandrofahur@gmail.com>
2020-08-15 10:53:36 -0700

    generators in es6 js

--- es6/iterators_generators.js
+++ es6/iterators_generators.js
@@ -1,19 +1,50 @@
-// Name iterator:
-function nameIterator(names) {
-  let nextIndex = 0;
+// // Name iterator:
+// function nameIterator(names) {
+//   let nextIndex = 0;
 
-  return {
-    next: function () {
-      return nextIndex < names.length ?
-        { value: names[nextIndex++], done: false } : { done: true }
-    }
+//   return {
+//     next: function () {
+//       return nextIndex < names.length ?
+//         { value: names[nextIndex++], done: false } : { done: true }
+//     }
+//   }
+// }
+
+// const namesArr = ['John', 'Jill', 'Jack'];
+// const names = nameIterator(namesArr);
+
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+
+// // Generators:
+// function* sayName() {
+//   yield 'John';
+//   yield 'Jill';
+//   yield 'Jack';
+// }
+
+// const names = sayName()
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+
+function* indexGenerator() {
+  let index = 0;
+
+  while (true) {
+    yield index++;
   }
 }
 
-const namesArr = ['John', 'Jill', 'Jack'];
-const names = nameIterator(namesArr);
-
-console.log(names.next());
-console.log(names.next());
-console.log(names.next());
-console.log(names.next());
+const gen = indexGenerator();
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
